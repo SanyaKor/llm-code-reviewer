860473ec193055043b7c5dd932671e6f4d0e2fbf
Aleksandr Lebedev <alexandr.2nd@gmail.com>
2025-10-08 18:57:36 +0200

    added gitignore file

---
+++ llm-code-reviewer/MainWindowView.cs
@@ -0,0 +1,67 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.IO;
+using System.Text.Json;
+using System;
+
+
+
+namespace AvaloniaApplication1
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _defaultText = "sadasdadassdfada";
+        private readonly string _initialText = "Текст по умолчанию";
+        public string DefaultText
+        {
+            get => _defaultText;
+            set
+            {
+                if (_defaultText != value)
+                {
+                    _defaultText = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private static string FilePath => Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            "AvaloniaApp",
+            "settings.json");
+
+        public void Save()
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(FilePath)!);
+            var json = JsonSerializer.Serialize(this);
+            File.WriteAllText(FilePath, json);
+        }
+
+        public void Restore()
+        {
+            DefaultText = _initialText;
+        }
+
+        public static MainViewModel Load()
+        {
+            try
+            {
+                if (File.Exists(FilePath))
+                {
+                    var json = File.ReadAllText(FilePath);
+                    return JsonSerializer.Deserialize<MainViewModel>(json) ?? new MainViewModel();
+                }
+            }
+            catch
+            {
+                
+            }
+
+            return new MainViewModel(); 
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string? name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
